5a63263d10c63acfabb76b1e25441955a9f467a3
Samuel Okoro Chka <okorosamuelchika@gmail.com>
2024-10-09 06:43:11 +0100

    fix hero texts

--- src/components/HeroTexts.tsx
+++ src/components/HeroTexts.tsx
@@ -6,8 +6,8 @@ const HeroTexts = () => {
       <h3 className="font-poppins text-2xl max-sm:text-xl max-lg:text-center">
         My Name is
       </h3>
-      <h1 className="font-rubik max-lg:text-center text-8xl tracking-tight md:name_underline text-primary max-sm:text-6xl ">
-        Okoro Samuel Chika
+      <h1 className="font-rubik text-center lg:text-left text-5xl name_underline lg:text-8xl tracking-tight text-primary">
+        Okoro Samuel <br /> Chika
       </h1>
       <TextRotator />
     </div>
